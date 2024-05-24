895a2c947b13e09f9ae3c6b90dcba77f6fa748bf
Vishnu Devarakonda <24705015+DevarakondaV@users.noreply.github.com>
2024-05-23 21:40:48 -0700

    working diff model

--- code/diffusion/model.py
+++ code/diffusion/model.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 import torch
 from torchvision.transforms.functional import center_crop
@@ -74,63 +75,92 @@ class SelfAttentionBlock(torch.nn.Module):
 
     def __init__(
             self,
+            in_channels,
             dimensions,
-            num_heads,
-            qkv_bias=False,
-            qk_scale=None,
-            drop=0,
-            attn_drop=0,
-            drop_path=0
+            DEVICE, 
+            DTYPE,
     )->None:
         super(SelfAttentionBlock, self).__init__()
+        self.t_proj = torch.nn.Linear(512, 512)
+        self.norm = torch.nn.GroupNorm(num_groups=32, num_channels=in_channels, eps=1e-6, affine=True)
+        self.q = torch.nn.Conv2d(in_channels, in_channels, kernel_size=1, stride=1, padding=0)
+        self.k = torch.nn.Conv2d(in_channels, in_channels, kernel_size=1, stride=1, padding=0)
+        self.v = torch.nn.Conv2d(in_channels, in_channels, kernel_size=1, stride=1, padding=0)
+        self.proj_out = torch.nn.Conv2d(in_channels, in_channels, kernel_size=1, stride=1, padding=0)
         self.flatten = torch.nn.Flatten(1)
-        # self.patch_projection = torch.nn.Linear(
-        #     patch_size * patch_size, dimensions)
-        self.position_embedding = SinPositionEmbedding(dimensions, 10)
-        # self.position_embedding.type(torch.bfloat16)
-        self.attn = Attention(
-            dimensions, num_heads, qkv_bias, qk_scale, attn_drop, drop)
-        self.drop_path = torch.nn.Identity()
-        self.norm1 = torch.nn.LayerNorm(dimensions)
-        self.MLP = torch.nn.Sequential(
-            torch.nn.Linear(dimensions, 4 * dimensions),
-            torch.nn.GELU(),
-            torch.nn.Dropout(drop),
-            torch.nn.Linear(4 * dimensions, dimensions),
-            torch.nn.Dropout(drop)
-        )
+        self.position_embedding = SinPositionEmbedding(dimensions, 10, DEVICE, DTYPE)
         self.norm2 = torch.nn.LayerNorm(dimensions)
 
     def forward(self, x, t, return_attention=False):
         t_enc = self.position_embedding(t)
-        x = self.flatten(x)
-        # x_p = self.patch_projection(x)
-        encoding = x + t_enc
-        y, attn = self.attn(self.norm1(encoding))
-        if return_attention:
-            return attn
-        x = x + self.drop_path(y)
-        x = x + self.drop_path(self.MLP(self.norm2(x)))
-        return x
+        # print(t_enc.dtype, self.t_proj.weight.dtype, self.t_proj.bias.dtype)
+        
+        # t_enc = self.t_proj(t_enc * torch.sigmoid(t_enc))
+        t_enc = self.t_proj(torch.relu(t_enc))
+        # t_enc = self.t_proj(t_enc)
+        t_enc = t_enc[:, None, None]
+        # print(x.shape, t_enc.shape)
+        e = x + t_enc
+        e = self.norm(e)
+        q = self.q(e)
+        k = self.k(e)
+        v = self.v(e)
+
+        b_s, c_s, h_s, w_s = q.shape
+        q = q.reshape(b_s, c_s, h_s * w_s)
+        q = q.permute(0, 2, 1)
+        k = k.reshape(b_s, c_s, h_s*w_s)
+        w = torch.bmm(q, k)
+        w = w * (int(c_s) ** (-0.5))
+        w = torch.nn.functional.softmax(w, dim=2)
+
+        v = v.reshape(b_s, c_s, h_s*w_s)
+        w = w.permute(0, 2, 1)
+        e = torch.bmm(v, w)
+        e = e.reshape(b_s, c_s, h_s, w_s)
+        
+        e = self.proj_out(e)
+        return x + e
 
 
 class SinPositionEmbedding(torch.nn.Module):
 
-    def __init__(self, D, T) -> None:
+    def __init__(self, D, T, DEVICE, DTYPE) -> None:
         super().__init__()
         self.D = D
         self.T = T
         self.N = 10000
+        self.DEVICE = DEVICE
+        self.DTYPE = DTYPE
 
     def forward(self, t):
-        i = torch.arange(self.D // 2)
-        embedding = torch.zeros(1, self.D, requires_grad=False)
-        embedding[:, 0::2] = torch.sin(t / (self.N ** (2 * i / self.D)))
-        embedding[:, 1::2] = torch.cos(t / (self.N ** (2 * i / self.D)))
+        i = torch.arange(self.D // 2).to(torch.device(self.DEVICE)).type(self.DTYPE)
+        # embedding = torch.zeros(1, self.D, requires_grad=False).to(torch.device('cuda'))# .type(torch.float16)
+        # print(t, self.N, i, self.D)
+        # embedding[:, 0::2] = torch.sin(t / (self.N ** (2 * i / self.D)))
+        # embedding[:, 1::2] = torch.cos(t / (self.N ** (2 * i / self.D)))
+        emb = []
+        for t_i in t:
+            embedding = torch.zeros(1, self.D, requires_grad=False).to(torch.device(self.DEVICE)).type(self.DTYPE)
+            embedding[:, 0::2] = torch.sin(t_i / (self.N ** (2 * i / self.D)))
+            embedding[:, 1::2] = torch.cos(t_i / (self.N ** (2 * i / self.D)))
+            emb.append(embedding.unsqueeze(0))
+        embedding = torch.cat(emb, dim=0).squeeze()
         return embedding
+    
+def get_timestep_embedding(timestep,  embedding_dim):
+    half_dim = embedding_dim // 2
+    emb = math.log(10000) / (half_dim - 1)
+    emb = torch.exp(torch.arange(half_dim) * -emb)
+    emb = emb.to(device=timestep.device)
+    emb = timestep.float()[:, None] * emb[None, :]
+    emb = torch.concat([torch.sin(emb), torch.cos(emb)], dim=1)
+    if embedding_dim % 2 == 1:
+        emb = torch.nn.functional.pad(emb, (0, 1, 0, 0 ))
+    return emb
 
 class UNet(torch.nn.Module):
-    def __init__(self, attn_dimension) -> None:
+    def __init__(self, attn_dimension, DEVICE, DTYPE) -> None:
         """
         Unet model for semantic segmentation. Input Size: 3 x 32 x 32
         Output: 3 x 32 x 32
@@ -139,7 +169,7 @@ class UNet(torch.nn.Module):
 
         self.print_shapes=False
         super(UNet, self).__init__()
-        self.block = SelfAttentionBlock(attn_dimension, 64)
+        self.block = SelfAttentionBlock(512, attn_dimension, DEVICE, DTYPE)
 
         self.down_block1=torch.nn.Sequential(
             torch.nn.Conv2d(1, 64, 3, padding=1, bias=False),
@@ -183,10 +213,10 @@ class UNet(torch.nn.Module):
 
         self.double_conv1=torch.nn.Sequential(
             torch.nn.Conv2d(512, 512, 3, padding=1, bias=False),
-            torch.nn.BatchNorm2d(512),
+            # torch.nn.BatchNorm2d(512),
             torch.nn.ReLU(inplace=True),
             torch.nn.Conv2d(512, 512, 3, padding=1, bias=False),
-            torch.nn.BatchNorm2d(512),
+            # torch.nn.BatchNorm2d(512),
             torch.nn.ReLU(inplace=True),
         )
 
@@ -239,6 +269,61 @@ class UNet(torch.nn.Module):
         tX, tY=(center_x - nWidth // 2), center_y - nHeight // 2
         return tensor[:, :, tX:tX + nWidth, tY:tY + nHeight]
 
+    def xforward(self, x, t):
+        out1 = self.down_block1(x)
+        x = self.mp1(out1)
+        out2 = self.down_block2(x)
+        x = self.mp2(out2)
+        out3 = self.down_block3(x)
+        x = self.mp3(out3)
+        out4 = self.down_block4(x)
+        x = self.mp4(out4)
+        out5 = self.double_conv1(x)
+        x = self.block(out5, t)
+        out5 = x.reshape(out5.shape)
+        x = self.upConv1(out5)
+        diffY = out4.size()[2] - x.size()[2]
+        diffX = out4.size()[3] - x.size()[3]
+        x = F.pad(
+            x,
+            [diffX // 2, diffX - diffX // 2,
+             diffY // 2, diffY - diffY // 2]
+        )
+        x = torch.concat([out4, x], axis=1)
+        x = self.double_conv2(x)
+        x = self.upConv2(x)
+        diffY = out3.size()[2] - x.size()[2]
+        diffX = out3.size()[3] - x.size()[3]
+        x = F.pad(
+            x,
+            [diffX // 2, diffX - diffX // 2,
+             diffY // 2, diffY - diffY // 2]
+        )
+        x = torch.concat([out3, x], axis=1)
+        x = self.double_conv3(x)
+        x = self.upConv3(x)
+        diffY = out2.size()[2] - x.size()[2]
+        diffX = out2.size()[3] - x.size()[3]
+        x = F.pad(
+            x,
+            [diffX // 2, diffX - diffX // 2,
+             diffY // 2, diffY - diffY // 2]
+        )
+        x = torch.concat([out2, x], axis=1)
+        x = self.double_conv4(x)
+        x = self.upConv4(x)
+        diffY = out1.size()[2] - x.size()[2]
+        diffX = out1.size()[3] - x.size()[3]
+        x = F.pad(
+            x,
+            [diffX // 2, diffX - diffX // 2,
+             diffY // 2, diffY - diffY // 2]
+        )
+        x = torch.concat([out1, x], axis=1)
+        x = self.double_conv5(x)
+        out = self.conv19(x)
+        return out
+
     def forward(self, x, t):
         
         out1=self.down_block1(x)
@@ -266,17 +351,12 @@ class UNet(torch.nn.Module):
         if self.print_shapes:
             print("M4: ", x.shape)
 
-        out5=self.double_conv1(x)
+        out5 = self.double_conv1(x)
 
         if self.print_shapes:
             print("Last: ", out5.shape)
 
-        # print("SSSSSSS B: ", out5.shape)
-        x = self.block(out5, t)
-        # print("XX: ", x.shape)
-        # out5 = x.squeeze(1).unsqueeze(-1).unsqueeze(-1)
-        out5 = x.reshape(out5.shape)
-        # print("SSSSSSS: ", x.shape)
+        out5 = self.block(out5, t)
 
         x=self.upConv1(out5)
         diffY=out4.size()[2] - x.size()[2]
@@ -327,7 +407,8 @@ class UNet(torch.nn.Module):
         x=torch.concat([out1, x], axis=1)
         x=self.double_conv5(x)
 
-        out=self.conv19(x)
+        out = self.conv19(x)
+
         if self.print_shapes:
             print("out: ", out.shape)
         return out
--- code/diffusion/utils.py
+++ code/diffusion/utils.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 import logging
-from model import UNet, SinPositionEmbedding, SelfAttentionBlock
+from model import UNet, SinPositionEmbedding, SelfAttentionBlock, get_timestep_embedding
 import torchvision
 import torch
 from torch import optim
@@ -9,10 +9,14 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from pathlib import Path
 import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
 
+DEVICE = torch.device("cuda")
+DTYPE = torch.float32
 
-def loss_fn(e, e_pred):
-    return ((e - e_pred)**2).mean()
+# def loss_fn(e, e_pred):
+#    return ((e - e_pred)**2).mean() # (dim=(1,2,3))
 
 
 def linear_scheduler(initial: float, final: float, T: int):
@@ -27,44 +31,6 @@ def linear_scheduler(initial: float, final: float, T: int):
     for i in range(0, T):
         yield initial + i * (final - initial) / T
 
-
-def sample():
-    SAMPLE_SIZE = [3, 32, 32]
-    DIMENSIONS = SAMPLE_SIZE[0] * SAMPLE_SIZE[1] * SAMPLE_SIZE[1]
-    BETA_INITIAL = 10E-4
-    BETA_FINAL = 0.02
-    T = 1000
-
-    model = UNet()
-    # model.type(torch.bfloat16)
-
-    X_t = torch.from_numpy(np.random.multivariate_normal(
-            [0 for i in range(DIMENSIONS)], 
-            np.identity(DIMENSIONS)
-        ).reshape(SAMPLE_SIZE))#.type(torch.bfloat16)
-    beta_values = linear_scheduler(BETA_INITIAL, BETA_FINAL, T)
-    for t in range(T, -1, -1):
-        beta_t = next(beta_values)
-        alpha_t = 1 - beta_t
-        sigma_t = beta_t
-
-        # z = torch.from_numpy(np.random.multivariate_normal(
-        #     [0 for i in range(DIMENSIONS)], 
-        #     np.identity(DIMENSIONS)
-        # ).reshape(SAMPLE_SIZE)).type(torch.bfloat16) if t > 1 else torch.from_numpy(np.zeros(SAMPLE_SIZE)).type(torch.bfloat16)
-        z = torch.from_numpy(np.random.multivariate_normal(
-            [0 for i in range(DIMENSIONS)], 
-            np.identity(DIMENSIONS)
-        ).reshape(SAMPLE_SIZE)) if t > 1 else torch.from_numpy(np.zeros(SAMPLE_SIZE))
-        z = z.unsqueeze(0)
-
-        factor = (1 - alpha_t) / ((1-alpha_t) **0.5)
-        pred = model(X_t.unsqueeze(0))
-        Xo = factor * pred
-        Xo =  (1 / (alpha_t)**0.5) * Xo
-        Xo += (sigma_t*z)
-    return Xo
-
 def get_alpha_bar_values(BETA_INITIAL, BETA_FINAL, T):
     vals = {}
     beta_values = linear_scheduler(BETA_INITIAL, BETA_FINAL, T)
@@ -74,23 +40,20 @@ def get_alpha_bar_values(BETA_INITIAL, BETA_FINAL, T):
     return vals
 
 def train(train_params, model):
-    DIR = "/Users/vishnudevarakonda/Documents/EngProjs/websites/DevarakondaV.github.io/tmp" # os.environ['WORKSPACE_DIR']
-    TMP_DIR = f'{DIR}/tmp/'
+    DIR = "/home/vishnu/Documents/EngProjs/websites/DevarakondaV.github.io/tmp" # os.environ['WORKSPACE_DIR']
+    TMP_DIR = f'{DIR}'
     logging.basicConfig(level=logging.INFO,
                         format='%(levelname)s: %(message)s')
     CHECKPOINT_DIR = f"{TMP_DIR}/results/diffusion/checkpoints"
-    BATCH_SIZE = 16
-    LEARNING_RATE = 2E-4
-    DEVICE = 'cpu'
+    BATCH_SIZE = train_params["batch_size"]
+    LEARNING_RATE = train_params["learning_rate"] # 1E-4
     CHECKPOINT = "NEW"
     OPTIMIZER = "Adam"
-    SAVE_CHECKPOINT = True
-    SAMPLE_SIZE = train_params["sample_size"]
     BETA_INITIAL = train_params["beta_initial"]
     BETA_FINAL = train_params["beta_final"]
+    EPOCH = train_params["epoch"]
     T = train_params["T"]
     ALPHA_BAR_VALUES = get_alpha_bar_values(BETA_INITIAL, BETA_FINAL, T)
-    DIMENSIONS = SAMPLE_SIZE[0] * SAMPLE_SIZE[1] * SAMPLE_SIZE[1]
 
     # dataset = torchvision.datasets.CIFAR10(
     #     "/tmp",
@@ -108,12 +71,13 @@ def train(train_params, model):
         True,
         download=True,
         transform=torchvision.transforms.Compose([
-            torchvision.transforms.Resize((32, 32)),
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize(
-            (0.1307,), (0.3081,))
+            # torchvision.transforms.Pad(2),
+            # torchvision.transforms.Resize((32, 32), torchvision.transforms.InterpolationMode.NEAREST),
+            torchvision.transforms.ToTensor(),
+            # torchvision.transforms.Normalize(0, 1),
+            # torchvision.transforms.Normalize((0.1307,), (0.3081,))
     ]))
-
+    
     loader_args = dict(batch_size=BATCH_SIZE,
                        num_workers=os.cpu_count(), pin_memory=True)
     train_loader = DataLoader(dataset, shuffle=True, **loader_args)
@@ -128,81 +92,113 @@ def train(train_params, model):
     ''')
 
     model.to(DEVICE)
+    # print([name for name,_ in model.named_parameters()])
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    # optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, 'min', patience=5, factor=0.9
+    )
 
-    stop = len(dataset) * 1000
-    minLoss = float('inf')
-    checkpoint_number = 0
-    i = 0
-    while True:
-        # Xo = np.identity(DIMENSIONS)
-        Xo,l = dataset[np.random.randint(0, len(dataset))]
-        # print(Xo.shape)
-        # t_step = np.random.uniform(0, T, size = 1)
-        t_step = np.random.randint(0, T)
-        alpha_bar_t = ALPHA_BAR_VALUES[t_step]
-        I = np.identity(DIMENSIONS)
-        e = np.random.multivariate_normal(
-            np.zeros(DIMENSIONS),
-            np.identity(DIMENSIONS)
-        ).reshape(SAMPLE_SIZE)
-        model_input = ((alpha_bar_t ** 0.5) * Xo) + ((1 - alpha_bar_t)**0.5) * e
-        model_input = model_input.type(torch.float32).unsqueeze(0)
-        e_pred = model(model_input, t_step).squeeze(0)
-        e = torch.from_numpy(e)
-        loss = loss_fn(e , e_pred)
-        optimizer.zero_grad(set_to_none=True)
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(
-            model.parameters(), 1.0)
-        optimizer.step()
-
-        if loss.item() < minLoss:
-            Path(CHECKPOINT_DIR).mkdir(parents=True, exist_ok=True)
-            state_dict=model.state_dict()
-            torch.save(state_dict, "{}/checkpoint_epoch0.pth".format(
-                CHECKPOINT_DIR, checkpoint_number))
-            logging.info(f'Checkpoint {checkpoint_number} saved!')
-            minLoss = loss.item()
-            checkpoint_number += 1
-        if stop == i:
-            break
-        i += 1
+    """
+    img,_ = dataset[0]
+    # img = img * 255
+    for t in range(0, T):
+        e = torch.randn_like(img).numpy()
+        alpha_bar_t = ALPHA_BAR_VALUES[t]
+        print(alpha_bar_t, e.mean())
+        model_input = ((alpha_bar_t ** 0.5) * img.numpy()) + ((1 - alpha_bar_t)**0.5) * e
+        if t == 0 or t == (T-1):
+            plt.figure(t)
+            plt.hist((model_input).flatten())
+            plt.savefig(f"/home/vishnu/Documents/EngProjs/websites/DevarakondaV.github.io/tmp/{t}_hist.jpg")
+            # fig, ax0 = plt.subplots(1, 1)
+            # ax0.hist(s_img.flatten())
+            # plt.savefig()
+        # if t % (T//50) == 0:
+        # s_img = (model_input*255).cpu().detach().float().numpy().astype(np.uint8).squeeze()
+        # s_img = (model_input).astype(np.uint8).squeeze()
+        print(model_input.mean(), model_input.std())
+        s_img = (model_input*255).squeeze()
+        cv2.imwrite(f"/home/vishnu/Documents/EngProjs/websites/DevarakondaV.github.io/tmp/sample{t}.jpg", s_img)
+        # break
     return
+    """
+    train_loader = list(train_loader)[:25]
+
+    for epoch in range(EPOCH):
+        epoch_loss = 0
+        model.train()
+        for i, (data, y) in enumerate(train_loader):
+            c = data.shape[0]
+            t_step = torch.randint(0, T, size=(c // 2 + 1,)) # np.random.randint(0, T, size = 1)[0]
+            t_step = torch.cat([t_step, T-t_step-1], dim=0)[:c]
+
+            alpha_bar_t = torch.from_numpy(np.array([ALPHA_BAR_VALUES[p.item()] for p in t_step])).reshape(c, 1, 1, 1)
+            e = torch.randn_like(data)
+            model_input = (alpha_bar_t.sqrt() * data) + (((1 - alpha_bar_t) ** 0.5) * e)
+            t_step = torch.from_numpy(np.array(t_step)).to(DEVICE).type(DTYPE)
+            
+            
+            model_input = model_input.to(DEVICE).type(DTYPE)
+            e = e.to(DEVICE).type(DTYPE)
+            e_pred = model(model_input, t_step)
+            loss = (e-e_pred).square().sum(dim=(1,2,3)).mean(dim=0) # torch.nn.functional.mse_loss(e, e_pred)
+            optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            optimizer.step()
+            logging.info(f'Epoch: {epoch}/{EPOCH}, i: {i}/{len(train_loader)}, Loss: {loss.item()}, LR: {optimizer.param_groups[0]["lr"]}')
+            epoch_loss += loss.item()
+
+        epoch_loss /= len(train_loader)
+        logging.info(f'EPOCH LOSS: {epoch_loss}')
+        scheduler.step(epoch_loss)
+
+        Path(CHECKPOINT_DIR).mkdir(parents=True, exist_ok=True)
+        state_dict=model.state_dict()
+        torch.save(state_dict, "{}/checkpoint_epoch{}.pth".format(
+            CHECKPOINT_DIR, epoch))
+        logging.info(f'Checkpoint {epoch} saved!, Loss: {loss.item()}')
+        model.eval()
+        sample(train_params, model)
 
 
 def sample(train_params, model):
     SAMPLE_SIZE = train_params["sample_size"]
     DIMENSIONS = SAMPLE_SIZE[0] * SAMPLE_SIZE[1] * SAMPLE_SIZE[1]
-    BETA_INITIAL = 10E-4
-    BETA_FINAL = 0.02
+    BETA_INITIAL = train_params["beta_initial"]
+    BETA_FINAL = train_params["beta_final"]
     T = train_params["T"]
     BETA_SCHEDULER = [i for i in linear_scheduler(BETA_INITIAL, BETA_FINAL, T)]
-    Xprev = np.random.multivariate_normal(
-        np.zeros(DIMENSIONS),
-        np.identity(DIMENSIONS)
-    ).reshape(SAMPLE_SIZE)
+    ALPHA_BAR_VALUES = get_alpha_bar_values(BETA_INITIAL, BETA_FINAL, T)
+    Xprev = torch.randn([1, 1, 28, 28])
     model.eval()
-    for t in range(T):
-        print("T: ", t)
+    for t in range(T-1, -1, -1):
+        # print("T: ", t)
         if t > 0:
-            z = np.random.multivariate_normal(np.zeros(DIMENSIONS), np.identity(DIMENSIONS)).reshape(SAMPLE_SIZE)
+            z = torch.randn_like(Xprev)
         else:
             z = 0
         beta_t = BETA_SCHEDULER[t]
         alpha_t = 1 - beta_t
-        sigma_t = 0
+        alpha_bar_t = ALPHA_BAR_VALUES[t]
+        sigma_t = beta_t**0.5
+        Xprev = Xprev.to(DEVICE).type(DTYPE)
         e_pred = model(
-            # torch.from_numpy(Xprev).type(torch.float16).unsqueeze(0),
-            torch.from_numpy(Xprev).type(torch.float32).unsqueeze(0),
-            t
-        ).squeeze(axis=0).detach().float().numpy()
-        scale_factor = (1 - alpha_t)/((1-alpha_t)**0.5)
+            Xprev,
+            torch.from_numpy(np.array([t])).to(DEVICE).type(DTYPE)
+        )
+        e_pred = e_pred.cpu().detach().float()
+        scale_factor = (1 - alpha_t)/((1-alpha_bar_t)**0.5)
+        Xprev = Xprev.cpu()
         Xn = ((1/(alpha_t**0.5)) * (Xprev - scale_factor * e_pred)) + sigma_t * z
-        Xprev = Xn # .squeeze(axis=0).detach().float().numpy()
-        if t == (T-1) or t % 50 == 0:
-            s_img = (Xn*255).astype(np.uint8).squeeze()
-            cv2.imwrite(f"/Users/vishnudevarakonda/Documents/EngProjs/websites/DevarakondaV.github.io/tmp/sample{t}.jpg", s_img)
+        Xprev = Xn
+        # s_img = (Xn*255).numpy().astype(np.uint8).squeeze()
+        s_img = (Xn*255).numpy().squeeze()
+        cv2.imwrite(f"/home/vishnu/Documents/EngProjs/websites/DevarakondaV.github.io/tmp/sample{t}.jpg", s_img)
+        if t == 0 or t == (T-1):
+            plt.figure(t)
+            plt.hist((Xn).flatten())
+            plt.savefig(f"/home/vishnu/Documents/EngProjs/websites/DevarakondaV.github.io/tmp/{t}_hist.jpg")
     return Xn
 
 
@@ -210,28 +206,108 @@ def get_diffusion_model_train_params(
         T: int,
         sample_size: list,
         beta_initial: float,
-        beta_final: float
+        beta_final: float,
+        batch_size: float,
     ):
     return {
         "sample_size": sample_size,
         "beta_initial": beta_initial,
         "beta_final": beta_final,
-        "T": T
+        "T": T,
+        "learning_rate": 1E-4,#E-4,
+        "epoch": 100,
+        "batch_size": batch_size
     }
 
+def make_joined_images():
+    tmp_dirs = "/home/vishnu/Documents/EngProjs/websites/DevarakondaV.github.io/tmp"
+    image_dirs = [f'{tmp_dirs}/{i}' for i in range(10)]
+    
+    """
+    zeros = [(int(i.split(".")[0].split("sample")[1]), cv2.imread(f'{tmp_dirs}/0/{i}')) for i in os.listdir(f'{tmp_dirs}/0')]
+    ones = [(int(i.split(".")[0].split("sample")[1]), cv2.imread(f'{tmp_dirs}/1/{i}')) for i in os.listdir(f'{tmp_dirs}/1')]
+    twos = [(int(i.split(".")[0].split("sample")[1]), cv2.imread(f'{tmp_dirs}/2/{i}')) for i in os.listdir(f'{tmp_dirs}/2')]
+    threes = [(int(i.split(".")[0].split("sample")[1]), cv2.imread(f'{tmp_dirs}/3/{i}')) for i in os.listdir(f'{tmp_dirs}/3')]
+    fours = [(int(i.split(".")[0].split("sample")[1]), cv2.imread(f'{tmp_dirs}/4/{i}')) for i in os.listdir(f'{tmp_dirs}/4')]
+    fives = [(int(i.split(".")[0].split("sample")[1]), cv2.imread(f'{tmp_dirs}/5/{i}')) for i in os.listdir(f'{tmp_dirs}/5')]
+    sixs = [(int(i.split(".")[0].split("sample")[1]), cv2.imread(f'{tmp_dirs}/6/{i}')) for i in os.listdir(f'{tmp_dirs}/6')]
+    sevens = [(int(i.split(".")[0].split("sample")[1]), cv2.imread(f'{tmp_dirs}/7/{i}')) for i in os.listdir(f'{tmp_dirs}/7')]
+    eights = [(int(i.split(".")[0].split("sample")[1]), cv2.imread(f'{tmp_dirs}/8/{i}')) for i in os.listdir(f'{tmp_dirs}/8')]
+    nines = [(int(i.split(".")[0].split("sample")[1]), cv2.imread(f'{tmp_dirs}/9/{i}')) for i in os.listdir(f'{tmp_dirs}/9')]
+    """
+
+    zeros = [(int(i.split(".")[0][6:]), cv2.imread(f'{tmp_dirs}/0/{i}')) for i in os.listdir(f'{tmp_dirs}/0')]
+    ones = [(int(i.split(".")[0][6:]), cv2.imread(f'{tmp_dirs}/1/{i}')) for i in os.listdir(f'{tmp_dirs}/1')]
+    twos = [(int(i.split(".")[0][6:]), cv2.imread(f'{tmp_dirs}/2/{i}')) for i in os.listdir(f'{tmp_dirs}/2')]
+    threes = [(int(i.split(".")[0][6:]), cv2.imread(f'{tmp_dirs}/3/{i}')) for i in os.listdir(f'{tmp_dirs}/3')]
+    fours = [(int(i.split(".")[0][6:]), cv2.imread(f'{tmp_dirs}/4/{i}')) for i in os.listdir(f'{tmp_dirs}/4')]
+    fives = [(int(i.split(".")[0][6:]), cv2.imread(f'{tmp_dirs}/5/{i}')) for i in os.listdir(f'{tmp_dirs}/5')]
+    sixs = [(int(i.split(".")[0][6:]), cv2.imread(f'{tmp_dirs}/6/{i}')) for i in os.listdir(f'{tmp_dirs}/6')]
+    sevens = [(int(i.split(".")[0][6:]), cv2.imread(f'{tmp_dirs}/7/{i}')) for i in os.listdir(f'{tmp_dirs}/7')]
+    eights = [(int(i.split(".")[0][6:]), cv2.imread(f'{tmp_dirs}/8/{i}')) for i in os.listdir(f'{tmp_dirs}/8')]
+    nines = [(int(i.split(".")[0][6:]), cv2.imread(f'{tmp_dirs}/9/{i}')) for i in os.listdir(f'{tmp_dirs}/9')]
+
+    # zeros = sorted(zeros, reverse=True, key = lambda x : x[0])
+    images = {i:[] for i in range(100)}
+    for i, img in zeros:
+        images[i].append(img)
+    for i, img in ones:
+        images[i].append(img)
+    for i, img in twos:
+        images[i].append(img)
+    for i, img in threes:
+        images[i].append(img)
+    for i, img in fours:
+        images[i].append(img)
+    for i, img in fives:
+        images[i].append(img)
+    for i, img in sixs:
+        images[i].append(img)
+    for i, img in sevens:
+        images[i].append(img)
+    for i, img in eights:
+        images[i].append(img)
+    for i, img in nines:
+        images[i].append(img)
+
+    images = {key:np.vstack([np.hstack(value[:5]), np.hstack(value[5:])]) for key, value in images.items()}
+    for key, value in images.items():
+        cv2.imwrite(f'{tmp_dirs}/images/{key}.jpg', value)
+
+def make_video():
+    tmp_dirs = "/home/vishnu/Documents/EngProjs/websites/DevarakondaV.github.io/tmp"
+    images_dir = f'{tmp_dirs}/images'
+    images = [(int(i.split(".")[0]), cv2.imread(f'{images_dir}/{i}')) for i in os.listdir(images_dir) if i != "gif"]
+    images = sorted(images, reverse=True, key = lambda x: x[0])
+    images = [Image.fromarray(img) for _,img in images]
+    for i in range(100):
+        images.append(images[-1])
+    img = images[0]
+    img.save(f'{images_dir}/gif/diffusion.gif', format='GIF', append_images=images[1:], save_all=True, duration=10,loop=0)
+
+
 if __name__ == "__main__":
-    # pE = SinPositionEmbedding(10, 10)
-    # pE.eval()
-    # print(pE(1))
+    # make_joined_images()
+    make_video()
+    exit()
+    """
+    pE = SinPositionEmbedding(10, 10)
+    pE.eval()
+    print(pE(1))
     # block = SelfAttentionBlock(10, 5)
     # print(block(torch.zeros(1, 2, 5), 0))
-    # exit()
+    
+    
+    print(get_timestep_embedding(torch.from_numpy(np.array([3])), 10))
+    exit()
+    """
+
     train_params = get_diffusion_model_train_params(
-        T = 1000,
-        # sample_size = [1, 28, 28],
-        sample_size = [1, 32, 32],
-        beta_initial = 10E-4,
-        beta_final = 0.02
+        T = 100,
+        sample_size = [1, 28, 28],
+        beta_initial=10E-4,
+        beta_final=0.02,
+        batch_size=64
     )
     # beta = linear_scheduler(BETA_INITIAL, BETA_FINAL, T)
     # for item in beta:
@@ -239,12 +315,33 @@ if __name__ == "__main__":
     # ALPHA_BAR_VALUES = get_alpha_bar_values(BETA_INITIAL, BETA_FINAL, T)
     # print(ALPHA_BAR_VALUES)
     model = UNet(
-        attn_dimension=512 * 2 * 2, #512
+        attn_dimension=512,
+        DEVICE=DEVICE, DTYPE=DTYPE
     )
+    """
+    model.to(DEVICE).type(DTYPE)
+    out = model(
+        torch.from_numpy(np.zeros((1, 1, 28, 28))).to(DEVICE).type(DTYPE),
+        torch.from_numpy(np.array([0])).to(DEVICE).type(DTYPE)
+    )
+    print(out.shape)
+    exit()
+    """
+    model.type(DTYPE).to(DEVICE)
     # model.type(torch.bfloat16)
-
-    model.state_dict(torch.load("/Users/vishnudevarakonda/Documents/EngProjs/websites/DevarakondaV.github.io/tmp/tmp/results/diffusion/checkpoints/checkpoint_epoch0.pth"))
-    x = sample(train_params, model)
 
     # train(train_params, model)
 
+    """
+    for param in torch.load("/home/vishnu/Documents/EngProjs/websites/DevarakondaV.github.io/tmp/results/diffusion/checkpoints/checkpoint_epoch937.pth"):
+        print(param)
+    print("PRINTING MODEL")
+    for param in model.named_parameters():
+        print(param[0])
+    exit()
+    """
+
+    # 82
+    # 90
+    model.load_state_dict(torch.load("/home/vishnu/Documents/EngProjs/websites/DevarakondaV.github.io/tmp/results/diffusion/checkpoints/checkpoint_epoch75.pth"))
+    x = sample(train_params, model)
